7c7af01c24b3e23447b5b08803611d8109421ba6
AsadullahBadhan <asadullah.badhan@gmail.com>
2021-10-06 00:45:19 +0600

    displaying user posts on UI

--- fetch.js
+++ fetch.js
@@ -1,13 +1,25 @@
 let userContainer = document.getElementById("user-container");
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
-    .then((data) => displayNames(data));
+    .then((data) => {
+        let first10 = data.slice(30, 35);
+        displayPosts(first10);
+    });
 
 function displayNames(users) {
     const names = users.map((user) => user.name);
     names.map((name) => {
         const li = document.createElement("li");
         li.innerText = name;
+        userContainer.appendChild(li);
+    });
+}
+
+function displayPosts(userPost) {
+    const posts = userPost.map((post) => post.body);
+    posts.map((post) => {
+        const li = document.createElement("li");
+        li.innerText = post;
         userContainer.appendChild(li);
     });
 }
